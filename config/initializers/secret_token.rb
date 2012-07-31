b0e1a238327307839ba6680825807aae54824883
Ludovic LAFFINEUR <ludovic.laffineur@gmail.com>
2012-07-31 15:50:52 +0200

    premier commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webkot::Application.config.secret_token = '9507e4d5037941916a2c32875ead475ef82d8d0be434d6938c20d00c7d99ccb1a75a7b0d911aeccbf91f63ea70efdafcd4ddf8ba691516a67133626c90f4d56c'
