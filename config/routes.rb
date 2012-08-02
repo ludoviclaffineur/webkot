53684d62f6b65f543f7d96ec890c64d2e630f653
Ludovic LAFFINEUR <ludovic.laffineur@gmail.com>
2012-08-03 00:52:17 +0200

    Youps

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Webkot::Application.routes.draw do
+
+
+  resources :posts
+
+  devise_for :users, :controllers => { :registrations => "registrations", :sessions => "sessions", :confirmations => "confirmations"}
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +56,7 @@ Webkot::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'posts#index'
 
   # See how all your routes lay out with "rake routes"
 
