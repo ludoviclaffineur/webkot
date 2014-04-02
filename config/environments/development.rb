ef81fef66f12350d11b786f11aad0014fd8d4e0c
Ludovic LAFFINEUR <ludovic.laffineur@gmail.com>
2014-04-02 21:50:45 +0200

    Update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,10 +30,11 @@ Webkot::Application.configure do
   config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Do not compress assets
-  config.assets.compress = false
+  config.assets.compress = true
 
   # Expands the lines which load the assets
   config.assets.debug = true
-
+  config.assets.enabled = true
+  
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
